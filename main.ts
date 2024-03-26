28b573bc0cbdde32d2a44485efe0622e7a037fa0
ffex <kurogan21@gmail.com>
2024-03-26 09:59:40 +0100

    Tutorial: added view

--- main.ts
+++ main.ts
@@ -1,15 +1,39 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
+import { ExampleView, VIEW_TYPE_EXAMPLE } from 'view';
 
 export default class MyPlugin extends Plugin {
 
 	async onload() {
-
+		this.registerView(
+			VIEW_TYPE_EXAMPLE,
+			(leaf) => new ExampleView(leaf)
+		  );
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('HelloWorld!');
-			//this.activateView();
+			this.activateView();
 		});
 	}
+
+	async activateView() {
+		const { workspace } = this.app;
+	
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE);
+	
+		if (leaves.length > 0) {
+		  // A leaf with our view already exists, use that
+		  leaf = leaves[0];
+		} else {
+		  // Our view could not be found in the workspace, create a new leaf
+		  // in the right sidebar for it
+		  leaf = workspace.getRightLeaf(false);
+		  await leaf.setViewState({ type: VIEW_TYPE_EXAMPLE, active: true });
+		}
+	
+		// "Reveal" the leaf in case it is in a collapsed sidebar
+		workspace.revealLeaf(leaf);
+	  }
 	
 }
