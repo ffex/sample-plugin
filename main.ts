cd275b3134dacf83de7e7223f3077565c93c8cb7
ffex <kurogan21@gmail.com>
2024-03-26 10:43:13 +0100

    Tutorial Implement React

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
 import { ExampleView, VIEW_TYPE_EXAMPLE } from 'view';
+import { RView, R_VIEW_TYPE_EXAMPLE } from 'r-view';
 
 export default class MyPlugin extends Plugin {
 
@@ -8,15 +9,26 @@ export default class MyPlugin extends Plugin {
 			VIEW_TYPE_EXAMPLE,
 			(leaf) => new ExampleView(leaf)
 		  );
+		  this.registerView(
+			R_VIEW_TYPE_EXAMPLE,
+			(leaf) => new RView(leaf)
+		  );
+
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('HelloWorld!');
-			this.activateView();
+			this.activateViewExample();
+			
+		});
+		const ribbonIconEl2 = this.addRibbonIcon('activity', 'React!', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			new Notice('React!');
+			this.activateViewReactExample();
 		});
 	}
 
-	async activateView() {
+	async activateViewExample() {
 		const { workspace } = this.app;
 	
 		let leaf: WorkspaceLeaf | null = null;
@@ -30,6 +42,25 @@ export default class MyPlugin extends Plugin {
 		  // in the right sidebar for it
 		  leaf = workspace.getRightLeaf(false);
 		  await leaf.setViewState({ type: VIEW_TYPE_EXAMPLE, active: true });
+		}
+	
+		// "Reveal" the leaf in case it is in a collapsed sidebar
+		workspace.revealLeaf(leaf);
+	  }
+	  async activateViewReactExample() {
+		const { workspace } = this.app;
+	
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(R_VIEW_TYPE_EXAMPLE);
+	
+		if (leaves.length > 0) {
+		  // A leaf with our view already exists, use that
+		  leaf = leaves[0];
+		} else {
+		  // Our view could not be found in the workspace, create a new leaf
+		  // in the right sidebar for it
+		  leaf = workspace.getRightLeaf(false);
+		  await leaf.setViewState({ type: R_VIEW_TYPE_EXAMPLE, active: true });
 		}
 	
 		// "Reveal" the leaf in case it is in a collapsed sidebar
---
+++ r-view.tsx
@@ -0,0 +1,36 @@
+import { StrictMode } from "react";
+import { ItemView, WorkspaceLeaf } from "obsidian";
+import { Root, createRoot } from "react-dom/client";
+import { ReactView } from "./ReactView";
+import React from "react";
+
+export const R_VIEW_TYPE_EXAMPLE = "react-example-view";
+
+export class RView extends ItemView {
+	root: Root | null = null;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType() {
+		return VIEW_TYPE_EXAMPLE;
+	}
+
+	getDisplayText() {
+		return "Example view";
+	}
+
+	async onOpen() {
+		this.root = createRoot(this.containerEl.children[1]);
+		this.root.render(
+			<StrictMode>
+				<ReactView />,
+			</StrictMode>,
+		);
+	}
+
+	async onClose() {
+		this.root?.unmount();
+	}
+}
